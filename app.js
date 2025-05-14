8a109ea5ddc0f74662946ce17cd85b51d24c0f93
Edisank <tjedisan2@gmail.com>
2025-05-14 09:51:28 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// JavaScript para el Panel de KavyGo
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Panel de KavyGo cargado correctamente.");
+});
+
+function navigateTo(page) {
+    window.location.href = page;
+}
