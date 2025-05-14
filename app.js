61c1b7eeb636d4cb549733b8adea55acaba77017
Edisank <tjedisan2@gmail.com>
2025-05-14 09:50:23 -0500

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Panel de KavyGo cargado correctamente.");
-});
-
-function navigateTo(page) {
-    window.location.href = page;
-}
